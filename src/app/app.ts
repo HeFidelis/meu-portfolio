8382effb3360e77371c622c22a77fe1d486ec12a
HeFidelis <heitorfidelis81@gmail.com>
2026-03-04 14:06:39 -0400

    feat: add smooth scroll behavior

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('meu-portfolio');
+  scroll(id: string) {
+    document.getElementById(id)?.scrollIntoView({behavior: 'smooth'});
+  }
 }
