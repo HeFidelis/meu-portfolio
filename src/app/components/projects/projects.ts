f1f47ae7736d8971065dd2fa219800cb45e40332
HeFidelis <heitorfidelis81@gmail.com>
2026-03-04 14:02:28 -0400

    feat: create dynamic project cards

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './projects.scss',
 })
 export class Projects {
-
+  projects = [
+  {
+    title: 'Projeto 1',
+    description: 'Descrição curta do projeto',
+    image: 'assets/img/project1.png',
+    link: 'https://github.com/seuusername/projeto1'
+  },
+  ];
 }
